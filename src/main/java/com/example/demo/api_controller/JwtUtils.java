9ffa85fc38e45340404d5d6b46cfd4d5077a0d8b
Joris <joris.sellam@gmail.com>
2022-12-22 10:04:48 +0100

    security: JwtUtils updated to the lastest version

--- src/main/java/com/example/demo/api_controller/JwtUtils.java
+++ src/main/java/com/example/demo/api_controller/JwtUtils.java
@@ -2,11 +2,12 @@ package com.example.demo.api_controller;
 
 import com.example.demo.model.GameUser;
 import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
-import java.security.SignatureException;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 @Component
@@ -19,20 +20,23 @@ public class JwtUtils {
 
     public String generateJwt(Authentication authentication) {
         GameUser user = (GameUser) authentication.getPrincipal();
-        return Jwts
-                .builder()
+
+        return Jwts.builder()
                 .setSubject(user.getUsername())
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(new Date().getTime() + this.expirationMs))
-                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .signWith(Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8)))
                 .compact();
     }
 
     public boolean validateToken(String token) {
         try {
-            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token);
+            Jwts.parserBuilder()
+                    .setSigningKey(Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8)))
+                    .build().parseClaimsJws(token);
             return true;
-
+//        } catch (SignatureException e) {
+//            System.err.println("Invalid JWT signature: {} " + e.getMessage());
         } catch (MalformedJwtException e) {
             System.err.println("Invalid JWT token: {} " + e.getMessage());
         } catch (ExpiredJwtException e) {
@@ -48,8 +52,9 @@ public class JwtUtils {
 
     public String getUsernameFromToken(String token) {
         return Jwts
-                .parser()
-                .setSigningKey(jwtSecret)
+                .parserBuilder()
+                .setSigningKey(Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8)))
+                .build()
                 .parseClaimsJws(token)
                 .getBody()
                 .getSubject();
